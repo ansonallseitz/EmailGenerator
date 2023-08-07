a8b63348ffe8e781423df3fd3618ea9aabf3684c
ansonallseitz <ansonallseitz@gmail.com>
2023-08-07 01:18:19 -0400

    Worked out some of the tedius windows contengency logic

--- emailgenerator.py
+++ emailgenerator.py
@@ -7,7 +7,17 @@ class EmailWindow:
         window.title("Email Generator")
 
         def DetermineOS():
-            print("OS was clicked")
+            selected = Os_Var.get()
+            if selected == 1:
+                WindowsContingencies()
+            elif selected == 2:
+                MacContingencies()
+
+        def WindowsContingencies():
+            print("You are using Windows")
+
+        def MacContingencies():
+            print("You are using Mac")
 
         # Title
         label1 = Label(window, text="This is the Email Generating Program")
@@ -35,27 +45,27 @@ class EmailWindow:
         label5 = Label(window, text="Operating system")
         Os_Var = IntVar()
         label5.grid(row=4, column=0, padx=10, sticky="w")
-        WindowsRadio = Radiobutton(window, text="windows", variable=Os_Var, value=1)
+        WindowsRadio = Radiobutton(window, text="Windows", variable=Os_Var, value=1)
         MacOSRadio = Radiobutton(window, text="MacOs", variable=Os_Var, value=2)
         WindowsRadio.grid(row=4, column=1, pady=5, sticky="w")
         MacOSRadio.grid(row=4, column=2, pady=5, sticky="w")
 
         # Dropdown box
-        label6 = Label(window, text= "what is the problem?")
+        label6 = Label(window, text="What is the problem?")
         label6.grid(row=5, column=0, padx=10, pady=5, sticky="w")
-        problems= ["Disk Encrypted", "OS Firewall Enabled", "OS Patched", "OS Supported"]
+        problems = ["Disk Encrypted", "OS Firewall Enabled", "OS Patched", "OS Supported"]
         ProblemDropdown = ttk.Combobox(window, values=problems)
         ProblemDropdown.grid(row=5, column=1, columnspan=2, pady=5, sticky="w")
-        
-        #Generate Email Button 
-        GenerateEmail = Button(window, text ="GenerateEmail", command=DetermineOS)
+
+        # Generate Email Button 
+        GenerateEmail = Button(window, text="Generate Email", command=DetermineOS)
         GenerateEmail.grid(row=6, column=3, padx=10, pady=10, sticky="w")
 
         # Email Output
         label7 = Label(window, text="Email Output")
-        label7.grid(row=6, column=0, padx=10, pady=5, sticky="w")
+        label7.grid(row=7, column=0, padx=10, pady=5, sticky="w")
         Email_Text = Text(window, borderwidth=2, relief="solid", height=10, width=60)
-        Email_Text.grid(row=7, column=0, columnspan=3, padx=10, pady=5)
+        Email_Text.grid(row=8, column=0, columnspan=3, padx=10, pady=5)
 
         window.mainloop()
 

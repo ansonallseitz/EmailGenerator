71dcd54909d01defa04efd458d220fc0aa40f633
Anson Allseitz <ansonallseitz@gmail.com>
2023-08-06 21:45:49 -0400

    Something is wrong with this grid system, its not compliling. But I know its close to correct so i'm commiting now

--- emailgenerator.py
+++ emailgenerator.py
@@ -8,40 +8,46 @@ class emailWindow():
         window = Tk()
         #Title 
         window.title("Email Generator") # set title
-        label1 = Label(text ="This is the Emal Generating Program")
-        label1.grid(row = 1, column =1)
-        
+        label1 = Label(window, text ="This is the Email Generating Program")
+        label1.grid(row = 0, column =1,columnspan=3, padx=10, pady=10)  
 
-        #Name Entry 
-        label2 = Label(text= "Persons first name")
-        NameEntry = Entry()
-        label2.grid(row =2, column =1)
-        NameEntry.grid(row =2, column =2)
+                
         
+      #Name Entry 
+        label2 = Label(window, text= "Persons first name")
+        NameEntry = Entry(window)
+        label2.grid(row=1, column =0, padx=10, sticky="w")
+        NameEntry.grid(row =1, column =1, padx=10, pady=5, columnspan=2, sticky="w")
+           
+
         #computer name entry 
-        label3 = Label(text= "Computer name")       
-        ComputerEntry = Entry()
-        label3.grid(row =3, column =1)
-        ComputerEntry.grid(row =3, column=2)
+        label3 = Label(window, text= "Computer name")       
+        ComputerEntry = Entry(window)
+        label3.grid(row =2, column =0, padx=10, sticky="w")
+        ComputerEntry.grid(row =2, column=1, padx=10, pady=5, columnspan=2, sticky="w")
 
         #OS Radio Button 
-        label4 = Label(text="Operating system")
+        label4 = Label(window, text="Operating system")
         Os_Var=IntVar()
-        label4.grid(row=4, column=1)
+        label4.grid(row=3, column=0, padx=10, sticky="w")
         WindowsRadio= Radiobutton(window, text ="windows", variable=Os_Var, value=1)
         MacOSRadio= Radiobutton(window, text="MacOs", variable=Os_Var, value=2 )
-        WindowsRadio.grid(row=4, column=2)
-        MacOSRadio.grid(row=4, column=3)
+        WindowsRadio.grid(row=3, column=1, pady=5, sticky="w")
+        MacOSRadio.grid(row=3, column=2, pady=5, sticky="w")
 
 
-      
-        
         #dropdown box
-        label5 = Label(text= "what is the problem?")
+        label5 = Label(window, text= "what is the problem?")
+        label5.grid(row=4, column=0, padx=10, pady=5, sticky="w")
         problems= ["Disk Encrypted", "OS Firewall Enabled", "OS Patched", "OS Supported"]
         ProblemDropdown = ttk.Combobox(window, values=problems )
-        ProblemDropdown.grid(row=5, column=2)
-        label5.grid(row=5, column=1)
+        ProblemDropdown.grid(row=4, column=1, columnspan=2, pady=5, sticky="w")        
+
+        #EmailOutput
+        label6 =Label(window, text= "Email Output")
+        label6.grid(row=5, column=0, padx=10, sticky="w")
+        Email_Text = Text(window, borderwidth=2, relief="solid", height=10, width=60 )
+        Email_Text.grid(row=6, column=0, columnspan=3, padx=10, pady=5)
     
 
         window.mainloop()
